64aca73599a4ed6e4b921500c4d46060610701b1
rizwanadam <riam16cs@cmrit.ac.in>
2017-02-25 18:36:01 +0530

    Update addcomp.c

--- week1/addcomp.c
+++ week1/addcomp.c
@@ -4,29 +4,31 @@ struct complex
 float real;
 float imaginary;
 };
-void addc(struct complex B[],int m)
+struct complex addc(struct complex B[],int m)
 {
+int i;
 struct complex C;
-for(int i=0;i<m;i++)
+for(i=0;i<m;i++)
 {
 C.real+=B[i].real;
 C.imaginary+=B[i].imaginary;
 }
-printf("The sum of %d complex numbers is:%f+%fi",m,C.real,C.imaginary);
+return C;
 }
 int main()
 {
-int n;
-struct complex A[100];
+int n,i;
+struct complex A[100],D;
 printf("Enter the number of complex numbers to be added");
 scanf("%d",&n);
 printf("Enter the real and imaginary part of %d complex numbers:",n);
-for(int i=0;i<n;i++)
+for(i=0;i<n;i++)
 {
 scanf("%f",&A[i].real);
 scanf("%f",&A[i].imaginary);
 }
-addc(A,n);
+D=addc(A,n);
+printf("The sum of %d complex numbers is:%f+%fi",n,D.real,D.imaginary);
 return 0;
 }
 
