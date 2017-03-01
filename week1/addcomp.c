1974b3991e11de60ac7613d2329632c46147b908
rizwanadam <riam16cs.@cmrit.ac.in>
2017-03-01 10:50:52 +0530

    Addition of n comolex numbers

--- week1/addcomp.c
+++ week1/addcomp.c
@@ -7,13 +7,23 @@ float imaginary;
 struct complex addc(struct complex B[],int m)
 {
 int i;
-struct complex C;
+struct complex c={0,0};
 for(i=0;i<m;i++)
 {
-C.real+=B[i].real;
-C.imaginary+=B[i].imaginary;
+c.real+=B[i].real;
+c.imaginary+=B[i].imaginary;}
+return c;
 }
-return C;
+struct complex getcomplex()
+{
+struct complex c;
+printf("Enter real and imaginary parts");
+scanf("%f%f",&c.real,&c.imaginary);
+return c;
+}
+void printcomplex(struct complex c)
+{
+printf("The sum of the complex numbers is %f+i%f",c.real,c.imaginary);
 }
 int main()
 {
@@ -21,14 +31,12 @@ int n,i;
 struct complex A[100],D;
 printf("Enter the number of complex numbers to be added");
 scanf("%d",&n);
-printf("Enter the real and imaginary part of %d complex numbers:",n);
 for(i=0;i<n;i++)
 {
-scanf("%f",&A[i].real);
-scanf("%f",&A[i].imaginary);
+A[i]=getcomplex();
 }
 D=addc(A,n);
-printf("The sum of %d complex numbers is:%f+%fi",n,D.real,D.imaginary);
+printcomplex(D);
 return 0;
 }
 
